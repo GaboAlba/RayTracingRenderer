f67be90eed3f6b83ae72222bd3fc657d00906b4a
Gabriel Alba <gabriel.f.alba@gmail.com>
2024-10-02 00:19:10 -0600

    Adding basic Ray

--- RayTracingRenderer/PPMImage/ImageGenerator.cs
+++ RayTracingRenderer/PPMImage/ImageGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using log4net;
+using RayTracingRenderer.Rays;
 using System.Numerics;
 
 namespace RayTracingRenderer.PPMImage
@@ -9,19 +10,47 @@ namespace RayTracingRenderer.PPMImage
         
         public static void RenderImage(int width, int height)
         {
-            Console.WriteLine("P3");
-            Console.WriteLine($"{width} {height}");
-            Console.WriteLine("255");
-            for (var j = 0; j < height; j++)
-            {
-                log.Debug($"\rScanlines remaining {height - j}\n"); //TODO: Make the bar show
-                for (var i = 0; i < width; i++)
-                {
-                    var red = (float)i / (width - 1);
-                    var green = (float)j / (height - 1);
-                    var blue = 0.0f;
+            var aspectRatio = 16.0f / 9.0f;
+            var imageWidth = 1920;
 
-                    var pixelColor = new Vector3(red, green, blue);
+            // Calculate height
+            var imageHeight = (int)(imageWidth / aspectRatio);
+            imageHeight = imageHeight < 1 ? 1 : imageHeight;
+
+            // Compute viewport size
+            var viewportHeight = 2.0f;
+            var viewportWidth = viewportHeight * (float)imageWidth / imageHeight;
+
+            // Compute camera
+            var focalLength = 1.0f;
+            var cameraCenter = new Vector3(0, 0, 0);
+
+            // Vertical and Horizontal viewport vectors
+            var viewportU = new Vector3(viewportWidth, 0, 0);
+            var viewportV = new Vector3(0, -viewportHeight, 0);
+
+            // Pixel delta
+            var pixelDeltaU = viewportU / imageWidth;
+            var pixelDeltaV = viewportV / imageHeight;
+
+            // Location of upper left pixel
+            var viewportUpperLeft = cameraCenter - new Vector3(0, 0, focalLength) - viewportU/2 - viewportV/2;
+            var pixel100_loc = viewportUpperLeft + 0.5f * (pixelDeltaU + pixelDeltaV);
+
+
+            Console.WriteLine("P3");
+            Console.WriteLine($"{imageWidth} {imageHeight}");
+            Console.WriteLine("255");
+            for (var j = 0; j < imageHeight; j++)
+            {
+                log.Debug($"\rScanlines remaining {imageHeight - j}\n"); //TODO: Make the bar show
+                for (var i = 0; i < imageWidth; i++)
+                {
+                    var pixelCenter = pixel100_loc + (i * pixelDeltaU) + (j * pixelDeltaV);
+                    var rayDirection = pixelCenter - cameraCenter;
+                    Ray ray = new Ray(cameraCenter, rayDirection);
+
+                    var pixelColor = Ray.RayColor(ray);
                     WriteColor(pixelColor);
                 }
             }
---
+++ RayTracingRenderer/Rays/Ray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracingRenderer.Rays
+{
+    public class Ray
+    {
+        private Vector3 Origin {  get; init; }
+
+        private Vector3 Direction { get; init; }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Ray() { }
+
+        /// <summary>
+        /// Creates a new instance of <cref=Ray>
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="direction"></param>
+        public Ray(Vector3 origin, Vector3 direction)
+        {
+            this.Origin = origin;
+            this.Direction = direction;
+        }
+
+        /// <summary>
+        /// Get the origin position for an specific ray
+        /// </summary>
+        /// <returns></returns>
+        public Vector3 GetOrigin() => this.Origin;
+
+        /// <summary>
+        /// Get the direction of an specific ray
+        /// </summary>
+        /// <returns></returns>
+        public Vector3 GetDirection() => this.Direction;
+
+        /// <summary>
+        /// Get the position of a ray precisely after a certain amount of time
+        /// </summary>
+        /// <param name="timePassed"></param>
+        /// <returns></returns>
+        public Vector3 GetPosition(float timePassed) => this.Origin + timePassed * this.Direction;
+
+        /// <summary>
+        /// Gets the ray color. TODO: Change it to the actual color
+        /// </summary>
+        /// <param name="ray"></param>
+        /// <returns></returns>
+        public static Vector3 RayColor(Ray ray)
+        {
+            return new Vector3(0, 0, 0);
+        }
+    }
+}
